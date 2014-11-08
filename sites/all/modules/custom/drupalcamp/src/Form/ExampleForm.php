140da95121be463af826d325dbbcacc69af49101
Joeri van Dooren <jvd@anaxis.be>
2014-11-08 20:34:13 +0100

    Rewrite module to be more OO.

---
+++ sites/all/modules/custom/drupalcamp/src/Form/ExampleForm.php
@@ -0,0 +1,49 @@
+<?php
+namespace Drupal\drupalcamp\Form;
+
+use Drupal\drupalcamp\Validator\TitleValidator;
+
+class ExampleForm {
+
+  public function buildMenu() {
+    return array(
+      'drupalcamp/example' => array(
+        'title' => 'DrupalCamp Example Form',
+        'description' => 'This page shows the DrupalCamp Example form.',
+        'page callback' => 'drupal_get_form',
+        'page arguments' => array('drupalcamp_example_form'),
+        'access arguments' => array('access administration pages'),
+        'weight' => 0,
+        'file' => 'includes/drupalcamp.forms.inc',
+      ),
+    );
+  }
+
+  public function buildForm($form, &$form_state) {
+    $form['title'] = array(
+      '#title' => t('Title'),
+      '#type' => 'textfield',
+      '#description' => t('It should be at least 5 characters long.'),
+      '#size' => 30,
+    );
+
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Save')
+    );
+
+    return $form;
+  }
+
+  public function validateForm($form, &$form_state) {
+    $title_validator = new TitleValidator();
+    if (!$title_validator->validate($form_state['values']['title'])) {
+      form_set_error('title', t('Title is too short!'));
+    }
+  }
+
+  public function submitForm($form, &$form_state) {
+    drupal_set_message(t('Form submission completed!'), 'status');
+    $form_state['redirect'] = '<front>';
+  }
+}
---
+++ sites/all/modules/custom/drupalcamp/src/Validator/TitleValidator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\drupalcamp\Validator;
+
+class TitleValidator {
+
+  public function validate($title) {
+    return strlen($title) > 5;
+  }
+}
+
